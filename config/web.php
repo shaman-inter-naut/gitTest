3aa8aafb5656ab1bf39c7a934edb362fce95cef2
shaman-inter-naut <65750511+shaman-inter-naut@users.noreply.github.com>
2020-05-22 10:09:48 +0500

    Test
    Sardorbek, web.php da cookieValidationKey ga qiymat berildi

--- config/web.php
+++ config/web.php
@@ -14,7 +14,7 @@ $config = [
     'components' => [
         'request' => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
-            'cookieValidationKey' => '',
+            'cookieValidationKey' => 'dsf5ds45f4ds564f54sd5f4ds5',
         ],
         'cache' => [
             'class' => 'yii\caching\FileCache',
